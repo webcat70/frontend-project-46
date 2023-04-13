1f31ec4ad971e6da6f540858d248afb23b004402
геннадий красняк <gkrasnyak2@gmail.com>
2023-04-13 19:58:41 +0300

    add files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,10 @@ import genDiff from '../src/index.js';
 const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
-  .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format', 'stylish')
+	.description('Compares two configuration files and shows a difference.')
+	.helpOption('-h, --help', 'output usage information')
+	.option('-f, --format <type>', 'output format', 'stylish')
+	.arguments('<filepath1> <filepath2>')
+	.action((file1, file2) => console.log(genDiff(file1, file2, program.opts().format)))
+	.action((filepath1, filepath2) => {console.log(genDiff(filepath1, filepath2));});
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-console.log('Hello World');
-console.log('Все получится');
+import _ from 'lodash';
+
+const genDiff = (file1, file2) => {
+	const keys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
+	console.log(keys)
+
+	const result = keys.map((key) => {
+		if (!_.has(file1[key]) && _.has(file2[key])) {
+			return { key, type: 'added', value: file2[key] };
+		}
+		if (_.has(file1[key]) && !_.has(file2[key])) {
+			return { key, type: 'deleted', value: file1[key] };
+		}
+		// if (_.has(file1[key]) && _.has(file2[key])) {
+		// 	return {key, type: '', value}
+		// }
+		if (file1[key] !== file2[key]) {
+			return { key, type: 'changed', valueBefore: file1[key], valueAfter: file2[key] };
+		}
+		return { key, type: 'unchanged', value: file1[key] };
+	});
+	return result;
+};
+genDiff(filepath1, filepath2)
+export default genDiff;
