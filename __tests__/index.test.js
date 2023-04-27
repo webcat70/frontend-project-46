c942bb1b19c99a9d53413571e55b89f66db000d7
геннадий красняк <gkrasnyak2@gmail.com>
2023-04-27 17:49:10 +0300

    failed test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,15 @@
-
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getData = (fullPath) => fs.readFileSync(fullPath, 'utf-8');
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('check default compare', () => {
+	expect(genDiff('__fixture__/filepath1.json', '__fixture__/filepath2.json')).toEqual(readFile('expectFilesStylish.txt'),
+	);
+});
--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import _ from 'lodash';
 import fs from 'fs';
 import path from 'node:path';
 import buildTree from './buildTree.js';
-//import parse from './parse.js';
+import parse from './parse.js';
 import stylish from './stylish.js';
 
 const getData = (filepath) => fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf8');
-const getFormat = (filepath) => filepath.extname(pathFile1).slice(1);
+const getTypeFile = (filepath) => path.extname(filepath).slice(1);
 
 const genDiff = (filepath1, filepath2) => {
-	const data1 = JSON.parse(getData(filepath1));
-	const data2 = JSON.parse(getData(filepath2));
+	const data1 = parse(getData(filepath1), getTypeFile(filepath1));
+	const data2 = parse(getData(filepath2), getTypeFile(filepath2));
 	return stylish(buildTree(data1, data2));
 };
 export default genDiff;
